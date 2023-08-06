065f658db3870bdbb154005aa463ab11c3abbc2b
Erik Simko <{{ .email | quote }}>
2023-08-06 18:36:53 +0000

    Update .chezmoiexternals/tpm.toml Update .config/tmux/tmux.conf Update install-install-homebrew.sh

--- run_once_before_install-install-homebrew.sh
+++ run_once_before_install-install-homebrew.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
-
-echo -e "Setting up Homebrew"
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+#!/bin/sh
+# Check to see if Homebrew is installed, and install it if it is not
+command -v brew >/dev/null 2>&1 || { echo >&2 "Installing Homebrew Now"; \
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"; }
