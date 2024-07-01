4123d32ef4c5150f515e64b948fb485f1c9c25e6
Erik Simko <erik.simko@bmw.de>
2024-07-01 13:09:41 +0200

    Font smoothing off only for Alacritty

--- .chezmoiscripts/darwin/run_onchange_after_macos_defaults.sh
+++ .chezmoiscripts/darwin/run_onchange_after_macos_defaults.sh
@@ -3,4 +3,4 @@
 set -eufo pipefail
 
 # better font rendering in Alacritty
-defaults -currentHost write -g AppleFontSmoothing -int 0
+defaults -currentHost write org.alacritty AppleFontSmoothing -int 0
