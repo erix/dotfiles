97d7f54b860df3e577e16b41e89715aebe0d939e
Erik Simko <erik.simko@gmail.com>
2026-07-02 23:19:58 +0200

    Polish WezTerm config

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -14,10 +14,31 @@ config.font = wezterm.font_with_fallback({
 	{ family = "Symbols Nerd Font Mono" },
 })
 config.font_size = 12.0
+config.line_height = 1.05
+config.cell_width = 1.0
+
+config.window_decorations = "RESIZE"
+config.native_macos_fullscreen_mode = true
+config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+	left = 10,
+	right = 10,
+	top = 8,
+	bottom = 8,
+}
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
 config.use_fancy_tab_bar = false
 config.tab_and_split_indices_are_zero_based = false
+config.tab_max_width = 32
+
+config.keys = {
+	{ key = "Enter", mods = "CMD", action = wezterm.action.ToggleFullScreen },
+	{ key = "t", mods = "CMD", action = wezterm.action.SpawnTab("CurrentPaneDomain") },
+	{ key = "w", mods = "CMD", action = wezterm.action.CloseCurrentTab({ confirm = false }) },
+	{ key = "d", mods = "CMD", action = wezterm.action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "d", mods = "CMD|SHIFT", action = wezterm.action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+}
 
 return config
