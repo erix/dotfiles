a73908b8449b149898f9141789c974fa02414197
Erik Simko <erik.simko@gmail.com>
2026-07-02 23:13:21 +0200

    Update terminal styling

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -6,13 +6,14 @@ config.color_scheme = "Catppuccin Macchiato"
 config.automatically_reload_config = true
 config.window_background_opacity = 0.88
 config.macos_window_background_blur = 20
-config.font = wezterm.font({
-	family = "RobotoMono Nerd Font",
-	-- stretch = "UltraExpanded",
-	-- weight = "Regular",
-	-- harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
+config.font_dirs = {
+	"/System/Applications/Utilities/Terminal.app/Contents/Resources/Fonts",
+}
+config.font = wezterm.font_with_fallback({
+	{ family = "SF Mono" },
+	{ family = "Symbols Nerd Font Mono" },
 })
-config.font_size = 13.0
+config.font_size = 12.0
 
 config.hide_tab_bar_if_only_one_tab = true
 config.tab_bar_at_bottom = false
